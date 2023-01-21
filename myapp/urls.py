dd86be2413195009c41db429bfc0a0e586878fd8
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-21 00:48:19 +0000

    criamos a troca de imagens do perfil

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('add_person/', views.add_person, name='add_person'),
+    path('profile/<int:person_id>/', views.profile, name='profile'),
+    path('update_profile/<int:person_id>/', views.update_profile, name='update_profile'),
 ]
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import Person
 
 def add_person(request):
@@ -9,4 +9,19 @@ def add_person(request):
         photo = request.FILES.get('photo')
         person = Person(name=name, photo=photo)
         person.save()
+        return redirect('profile', person_id=person.id)
     return render(request, 'add_person.html')
+
+def profile(request, person_id):
+    person = Person.objects.get(id=person_id)
+    return render(request, 'profile.html', {'person':person})
+
+def update_profile(request, person_id):
+    person = Person.objects.get(id=person_id)
+    if request.method == 'POST':
+        person.name = request.POST.get('name')
+        if request.FILES.get('photo'):
+            person.photo = request.FILES.get('photo')
+        person.save()
+        return redirect('profile', person_id=person_id)
+    return render(request, 'update_profile.html', {'person': person})
